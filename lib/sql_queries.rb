f1fa0c78531d2bc5cd475c716223a4f099d8f6d1
deniznida <denizunat@hotmail.com>
2015-10-12 21:02:03 -0400

    fixes typo in sql_queries

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -22,7 +22,7 @@ def selects_most_prominent_color_and_returns_with_count
   "Write your SQL query here"
 end
 
-def counts_number_of_bears_with_goofy_temperments 
+def counts_number_of_bears_with_goofy_temperaments 
   "Write your SQL query here"
 end
 
