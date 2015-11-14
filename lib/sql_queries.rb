5b2f12ebf2d666c8aeacf708ca192b8713a4d26a
Joe Burgess <joe@flatironschool.com>
2015-11-14 09:07:39 -0500

    bringing solution back in

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,31 +1,31 @@
-def selects_all_female_bears_return_name_and_age
-  "Write your SQL query here"
+def selects_all_female_bears_return_name_and_age 
+  "SELECT bears.name, bears.age FROM bears WHERE gender='F';"
 end
 
-def selects_all_bears_names_and_orders_in_alphabetical_order
-  "Write your SQL query here"
+def selects_all_bears_names_and_orders_in_alphabetical_order 
+  "SELECT bears.name FROM bears ORDER BY bears.name ASC;"
 end
 
-def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "Write your SQL query here"
+def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest 
+  "SELECT bears.name, bears.age FROM bears WHERE alive=1 ORDER BY bears.age ASC;"
 end
 
-def selects_oldest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+def selects_oldest_bear_and_returns_name_and_age 
+  "SELECT bears.name, bears.age FROM bears ORDER BY bears.age DESC LIMIT 1;"
 end
 
-def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+def select_youngest_bear_and_returns_name_and_age 
+  "SELECT bears.name, bears.age FROM bears ORDER BY bears.age ASC LIMIT 1;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "SELECT bears.color, COUNT(bears.color) FROM bears GROUP BY bears.color ORDER BY COUNT(*) DESC LIMIT 1;"
 end
 
-def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+def counts_number_of_bears_with_goofy_temperaments 
+  "SELECT COUNT(bears.temperament) FROM bears WHERE bears.temperament='goofy';"
 end
 
-def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+def selects_bear_that_killed_Tim 
+  "SELECT * FROM bears WHERE bears.name IS NULL;"
 end
