f9e333ef1e69643343435cd5baf86bb5a6914e00
Jean Gérard Bousiquot <jgb@noukod.com>
2018-11-05 20:27:30 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,47 @@
+INSERT INTO bears (
+  name, age, gender, color, temperament, alive
+) VALUES (
+  "Mr. Chocolate", 3, "Male", "Black", "Gay", 1
+);
+
+INSERT INTO bears (
+  name, age, gender, color, temperament, alive
+) VALUES (
+  "Rowdy", 1, "Male", "Brown", "Serious", 1
+);
+
+INSERT INTO bears (
+  name, age, gender, color, temperament, alive
+) VALUES (
+  "Tabitha", 5, "Female", "Red", "Angry", 1
+);
+
+INSERT INTO bears (
+  name, age, gender, color, temperament, alive
+) VALUES (
+  "Sergeant Brown", 10, "Male", "Brown", "Happy", 0
+);
+
+INSERT INTO bears (
+  name, age, gender, color, temperament, alive
+) VALUES (
+  "Melissa", 6, "Female", "Pink", "Gay", 1
+);
+
+INSERT INTO bears (
+  name, age, gender, color, temperament, alive
+) VALUES (
+  "Grinch", 4, "Male", "Black", "Gay", 1
+);
+
+INSERT INTO bears (
+  name, age, gender, color, temperament, alive
+) VALUES (
+  "Wendy", 5, "Female", "Brown", "Angry", 1
+);
+
+INSERT INTO bears (
+  name, age, gender, color, temperament, alive
+) VALUES (
+  NULL, 3, "Male", "Grey", "Nervous", 1
+);
