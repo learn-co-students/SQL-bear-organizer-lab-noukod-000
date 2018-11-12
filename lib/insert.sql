0fafaf3374d29c97bc22bedb4851b621ee0e470c
Gilbert Torchon <gilbert@noukod.com>
2018-11-12 15:23:30 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Mr. Chocolate',8,'FEMALE','Brown','Choleric','TRUE');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Rowdy',34,'FEMALE','Brown','Sanguine','TRUE');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Tabitha',27,'MALE','Brown','Phlegmatic','FALSE');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Sergeant Brown',10,'MALE','Brown','Choleric','FALSE');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Melissa',11,'MALE','Brown','Choleric','FALSE');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Grinch',16,'MALE','Brown','Sanguine','TRUE');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Wendy',20,'MALE','Brown','Choleric','FALSE');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES (NULL,28,'MALE','Brown','Choleric','FALSE');
